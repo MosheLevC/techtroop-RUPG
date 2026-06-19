b937a8ecf4ae500daea47d9b8e70848652608935
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-19 14:43:06 +0300

    add generateFriends function to fetch user friends data

--- model.js
+++ model.js
@@ -26,7 +26,29 @@ const generateUser = async () => {
     });
 };
 
+const generateFriends = async () => {
+  axios
+    .get("https://randomuser.me/api/?inc=name&results=6&noinfo")
+    .then(({ data }) => {
+      if (data) {
+        const results = data.results;
+        userData.userFriends = results.map((friend) => ({
+          fName: friend.name.first,
+          lName: friend.name.last,
+        }));
+      } else {
+        userData.userFriends = null;
+
+        throw new Error("fetch user data failed");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 export const getUserInfo = async () => {
   // note to self need to turn into a promise all type thing so it gets it in parralel
   await generateUser();
+  await generateFriends();
 };
