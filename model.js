0b8c3ff8b3e67bf2a3662d231d6071511ac992a8
Moshe Lev Ceder <navkc11@gmail.com>
2026-06-19 14:41:57 +0300

    refactor user data handling in model.js for clarity and consistency

--- model.js
+++ model.js
@@ -1,24 +1,22 @@
 import axios from "axios";
 
-let userData = {};
+const userData = {};
 const generateUser = async () => {
   axios
-    .get("https://randomuser.me/api/?inc=picture,name,location")
+    .get("https://randomuser.me/api/?inc=picture,name,location&noinfo")
     .then(({ data }) => {
       if (data) {
         const result = data.results?.[0];
-        const userInfo = {};
 
-        userInfo["userPictureUrl"] = result.picture.thumbnail;
-        userInfo["fNAme"] = result.name.first;
-        userInfo["lName"] = result.name.last;
-        userInfo["city"] = result.location.city;
-        userInfo["state"] = result.location.state;
-
-        userData["userInfo"] = userInfo;
+        userData.userInfo = {
+          userPictureUrl: result.picture.thumbnail,
+          fNAme: result.name.first,
+          lName: result.name.last,
+          city: result.location.city,
+          state: result.location.state,
+        };
       } else {
-        userData["userInfo"] = null;
-        console.log("blep");
+        userData.userInfo = null;
 
         throw new Error("fetch user data failed");
       }
@@ -29,6 +27,6 @@ const generateUser = async () => {
 };
 
 export const getUserInfo = async () => {
-  // need to turn into a promise all type thing so it gets it in parralel
+  // note to self need to turn into a promise all type thing so it gets it in parralel
   await generateUser();
 };
